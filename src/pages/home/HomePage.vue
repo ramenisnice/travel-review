b417d1524a2a83a203dcd6eb26c0d298f957f4db
Yue Xiong <xiongyueee@outlook.com>
2022-08-31 21:02:25 +0800

    finish best sale recommend

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -2,14 +2,16 @@
   <HomeHeader />
   <HomeSwiper />
   <HomeIcons />
+  <HomeRecommend />
 </template>
 
 <script>
 import HomeHeader from "./components/HomeHeader.vue";
 import HomeSwiper from "./components/HomeSwiper.vue";
 import HomeIcons from "./components/HomeIcons.vue";
+import HomeRecommend from "./components/HomeRecommend.vue";
 
 export default {
-  components: { HomeHeader, HomeSwiper, HomeIcons },
+  components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecommend },
 };
 </script>
