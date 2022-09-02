36dd6cd59e180956d00494d7c94b3bda4cb33192
Yue Xiong <xiongyueee@outlook.com>
2022-09-03 02:14:16 +0800

    add alphabet index ui

--- src/pages/city/CityPage.vue
+++ src/pages/city/CityPage.vue
@@ -2,13 +2,15 @@
   <CityHeader />
   <CitySearch />
   <CityList />
+  <CityAlphabet />
 </template>
 
 <script>
 import CityHeader from "./components/CityHeader.vue";
 import CitySearch from "./components/CitySearch.vue";
 import CityList from "./components/CityList.vue";
+import CityAlphabet from "./components/CityAlphabet.vue";
 export default {
-  components: { CityHeader, CitySearch, CityList },
+  components: { CityHeader, CitySearch, CityList, CityAlphabet },
 };
 </script>
---
+++ src/pages/city/components/CityAlphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="alphabet">
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.list
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    display: flex
+    flex-direction: column
+    justify-content: center
+    .item
+        line-height: .4rem
+        text-align: center
+        color: #00bcd4
+</style>
